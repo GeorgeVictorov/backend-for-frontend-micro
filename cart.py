323d9a5d6c0d36bf23826bc1a524f0494b01ad99
georgiiviktorov <nowayisleft@gmail.com>
2025-03-10 23:29:52 +0400

    cart fix + bff

--- cart.py
+++ cart.py
@@ -12,15 +12,15 @@ routes = web.RouteTableDef()
 
 
 @routes.get('/users/{id}/cart')
-async def favorites(request: Request) -> Response:
+async def cart(request: Request) -> Response:
     try:
         str_id = request.match_info['id']
         user_id = int(str_id)
         db = request.app[DB_KEY]
         favorite_query = 'SELECT product_id from user_cart where user_id = $1'
         result = await db.fetch(favorite_query, user_id)
-        if result:
-            return web.json_response(dict(record) for record in result)
+        if result is not None:
+            return web.json_response([dict(record) for record in result])
         else:
             raise web.HTTPNotFound()
     except ValueError:
